3a17e42cf5e5612dfc8bd333d0d2cf9c94740c75
Vinicius D. de Mattos <viniciusmattos01@gmail.com>
2019-11-30 18:31:09 -0300

    Atualizando

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-echo	"Olá Mundo 456
+echo	"Olá Mundo 456";
 
-echo "Olá Udemy"
-=======
-echo	"Olá Mundo"
-  
-  echo "Olá Hcode"
->>>>>>> 94ae4fe9ad958eac099ab6d6c85d8b22ec98bafb
+
+echo "Cursando PHP 7";
